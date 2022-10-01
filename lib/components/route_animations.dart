80ec97c9d457c38f8374d4086d5fb13d27edb202
HBA114 <hbasriayhaner114@gmail.com>
2022-10-02 02:50:42 +0300

    Closed Animations

--- lib/components/route_animations.dart
+++ lib/components/route_animations.dart
@@ -1,37 +1,37 @@
-import 'package:flutter/cupertino.dart';
-import 'package:hba114_github_io_website/screens/about.dart';
-import 'package:hba114_github_io_website/screens/home.dart';
-import 'package:hba114_github_io_website/screens/socials.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:hba114_github_io_website/screens/about.dart';
+// import 'package:hba114_github_io_website/screens/home.dart';
+// import 'package:hba114_github_io_website/screens/socials.dart';
 
-class RouteHomeAnimation extends CupertinoPageRoute {
-  RouteHomeAnimation() : super(builder: (BuildContext context) => new Home());
+// class RouteHomeAnimation extends CupertinoPageRoute {
+//   RouteHomeAnimation() : super(builder: (BuildContext context) => new Home());
 
-  // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
-  @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    return new FadeTransition(opacity: animation, child: new Home());
-  }
-}
+//   // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
+//   @override
+//   Widget buildPage(BuildContext context, Animation<double> animation,
+//       Animation<double> secondaryAnimation) {
+//     return new FadeTransition(opacity: animation, child: new Home());
+//   }
+// }
 
-class RouteAboutAnimation extends CupertinoPageRoute {
-  RouteAboutAnimation() : super(builder: (BuildContext context) => new About());
+// class RouteAboutAnimation extends CupertinoPageRoute {
+//   RouteAboutAnimation() : super(builder: (BuildContext context) => new About());
 
-  // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
-  @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    return new FadeTransition(opacity: animation, child: new About());
-  }
-}
+//   // OPTIONAL IF YOU WISH TO HAVE SOME EXTRA ANIMATION WHILE ROUTING
+//   @override
+//   Widget buildPage(BuildContext context, Animation<double> animation,
+//       Animation<double> secondaryAnimation) {
+//     return new FadeTransition(opacity: animation, child: new About());
+//   }
+// }
 
-class RouteSocialsAnimation extends CupertinoPageRoute {
-  RouteSocialsAnimation()
-      : super(builder: (BuildContext context) => new Socials());
+// class RouteSocialsAnimation extends CupertinoPageRoute {
+//   RouteSocialsAnimation()
+//       : super(builder: (BuildContext context) => new Socials());
 
-  @override
-  Widget buildPage(BuildContext context, Animation<double> animation,
-      Animation<double> secondaryAnimation) {
-    return new FadeTransition(opacity: animation, child: new Socials());
-  }
-}
+//   @override
+//   Widget buildPage(BuildContext context, Animation<double> animation,
+//       Animation<double> secondaryAnimation) {
+//     return new FadeTransition(opacity: animation, child: new Socials());
+//   }
+// }
